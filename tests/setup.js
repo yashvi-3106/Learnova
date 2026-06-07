2a579fbcda0b5a7a4df12c969780a250b850cabb
unknown <paridhijain153@gmail.com>
2026-06-07 10:23:01 +0530

    test: add AmbientMode component test

--- tests/setup.js
+++ tests/setup.js
@@ -2,9 +2,7 @@ import { webcrypto } from "node:crypto";
 import "@testing-library/jest-dom/vitest";
 import { TextEncoder, TextDecoder } from "util";
 import { ReadableStream } from "node:stream/web";
-import { vi } from "vitest";
 
-globalThis.jest = vi;
 if (typeof globalThis.crypto === "undefined") {
   globalThis.crypto = webcrypto;
 }
